e904f34a44cb84c3fa45f2a513dbb1b38c2334b1
akylbekova97 <ggdddrrr172@gmail.com>
2025-05-22 01:00:24 +0600

    feature/productCard

--- src/modules/product-of-the-day/ui/ProductOfTheDay.jsx
+++ src/modules/product-of-the-day/ui/ProductOfTheDay.jsx
@@ -1,24 +1,34 @@
-import { BannerImg } from '../../../shared/assets/imgs/BannerImg.jsx'
-import { AppLink, Title } from '../../../shared/components/ui/Text'
-
-import s from './ProductOfTheDay.module.scss'
+import { BannerImg } from "../../../shared/assets/imgs/BannerImg.jsx";
+import { AppLink } from "../../../shared/components/ui/AppLink";
+import { Title } from "../../../shared/components/ui/Text";
+import { ProductCard } from "../../Product-components/ProductCard/ui/ProductCard.jsx";
+import s from "./ProductOfTheDay.module.scss";
 
 export function ProductOfTheDay() {
-	return (
-		<div className={s.container}>
-			<div className={s.link}>
-				<AppLink size='md-16'>Телевизоры</AppLink>
-				<AppLink size='md-16'>Бытовая техника</AppLink>
-				<AppLink size='md-16'>Встраиваемая бытовая техника</AppLink>
-				<AppLink size='md-16'>Мелкая бытовая техника</AppLink>
-			</div>
+  return (
+    <div className={s.container}>
+      <div className={s.link}>
+        <AppLink size="md-16">Телевизоры</AppLink>
+        <AppLink size="md-16">Бытовая техника</AppLink>
+        <AppLink size="md-16">Встраиваемая бытовая техника</AppLink>
+        <AppLink size="md-16">Мелкая бытовая техника</AppLink>
+      </div>
 
-			<BannerImg />
+      <div className={s.img}>
+        <BannerImg />
+      </div>
 
-			<div>
-				<Title size='md-20'>Товар дня</Title>
-				<div className={s.product}></div>
-			</div>
-		</div>
-	)
+      <div className={s.product}>
+        <Title size="md-20">Товар дня</Title>
+        <ProductCard
+          id="2"
+          img="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_FWF2judaujT30K9sMf-tZFhMWpgP6xCemw&s"
+          title="Пылесос Tefal Super"
+          price="17990"
+          oldPrice="19990"
+          promotion="-10%"
+        />
+      </div>
+    </div>
+  );
 }
